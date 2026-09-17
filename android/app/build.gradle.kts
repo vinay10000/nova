80919ef3a31c45b68a094009dc4476de6822a5a1
Vinay <127865962+vinay10000@users.noreply.github.com>
2026-09-17 20:17:05 +0530

    feat(android): improve Nova design system and navigation UX

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -50,7 +50,7 @@ android {
     sourceCompatibility = JavaVersion.VERSION_17
     targetCompatibility = JavaVersion.VERSION_17
   }
-  kotlin { compilerOptions { jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVMTarget.JVM_17) } }
+  kotlin { compilerOptions { jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17) } }
 }
 
 dependencies {
